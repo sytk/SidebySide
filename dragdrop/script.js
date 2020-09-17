9ac44020ff123fa5961ff4203d07c89fe08ac43e
sytk <33809675+sytk@users.noreply.github.com>
2020-09-17 16:12:24 +0900

    pdfのサイズ変更

--- dragdrop/script.js
+++ dragdrop/script.js
@@ -4,7 +4,6 @@ window.onload = () => {
   const video  = document.querySelector("#camera");
   const canvas = document.querySelector("#picture");
 
-
   document.getElementById('file-sample').addEventListener('change', function (e) {
       // 1枚だけ表示する
       var file = e.target.files[0];
@@ -41,20 +40,9 @@ window.onload = () => {
   .catch( (err) => {
     console.log(err.name + ": " + err.message);
   });
-
-  // /**
-  // * シャッターボタン
-  // */
-  // document.querySelector("#shutter").addEventListener("click", () => {
-  //
-  // // 演出的な目的で一度映像を止めてSEを再生する
-  // video.pause();  // 映像を停止
-  // setTimeout( () => {
-  //   video.play();    // 0.5秒後にカメラ再開
-  // }, 500);
-  // });
-
 }
+
+var ratio = 0.75;
 
 interact('.resize-drag')
   .resizable({
@@ -68,10 +56,20 @@ interact('.resize-drag')
         var y = (parseFloat(target.getAttribute('data-y')) || 0)
 
         // update the element's style
-        target.style.width = event.rect.width + 'px'
-        target.style.height = event.rect.height + 'px'
+        // target.style.width = event.rect.width + 'px'
+        // target.style.height = event.rect.height + 'px'
 
-        // translate when resizing from top or left edges
+        if (Math.abs(event.deltaRect.left) < Math.abs(event.deltaRect.top))
+        {
+          target.style.width = event.rect.height / ratio + 'px'
+          target.style.height = event.rect.height + 'px'
+        }
+        else
+        {
+          target.style.width = event.rect.width  + 'px'
+          target.style.height = event.rect.width * ratio + 'px'
+        }
+
         x += event.deltaRect.left
         y += event.deltaRect.top
 
@@ -126,3 +124,127 @@ function dragMoveListener (event) {
 
 // this function is used later in the resizing and gesture demos
 window.dragMoveListener = dragMoveListener
+
+
+    var __PDF_DOC,
+        __CURRENT_PAGE,
+        __TOTAL_PAGES,
+        __PAGE_RENDERING_IN_PROGRESS = 0,
+        __CANVAS = $('#pdf-canvas').get(0),
+        __CANVAS_CTX = __CANVAS.getContext('2d');
+
+    function showPDF(pdf_url) {
+        // $("#pdf-loader").show();
+
+        PDFJS.getDocument({ url: pdf_url }).then(function(pdf_doc) {
+            __PDF_DOC = pdf_doc;
+            __TOTAL_PAGES = __PDF_DOC.numPages;
+
+            // Hide the pdf loader and show pdf container in HTML
+            $("#pdf-loader").hide();
+            $("#pdf-contents").show();
+            $("#pdf-total-pages").text(__TOTAL_PAGES);
+
+            // Show the first page
+            showPage(1);
+        }).catch(function(error) {
+            // If error re-show the upload button
+            $("#pdf-loader").hide();
+            $("#upload-button").show();
+
+            alert(error.message);
+        });;
+    }
+
+    function showPage(page_no) {
+        __PAGE_RENDERING_IN_PROGRESS = 1;
+        __CURRENT_PAGE = page_no;
+
+        // Disable Prev & Next buttons while page is being loaded
+        $("#pdf-next, #pdf-prev").attr('disabled', 'disabled');
+
+        // While page is being rendered hide the canvas and show a loading message
+        $("#pdf-canvas").hide();
+        $("#page-loader").show();
+        $("#download-image").hide();
+
+        // Update current page in HTML
+        $("#pdf-current-page").text(page_no);
+
+        // Fetch the page
+        __PDF_DOC.getPage(page_no).then(function(page) {
+            // As the canvas is of a fixed width we need to set the scale of the viewport accordingly
+            var scale_required = __CANVAS.width / page.getViewport(1).width;
+
+            // Get viewport of the page at required scale
+            var viewport = page.getViewport(scale_required);
+
+            // Set canvas height
+            __CANVAS.height = viewport.height;
+
+            var renderContext = {
+                canvasContext: __CANVAS_CTX,
+                viewport: viewport
+            };
+
+            // Render the page contents in the canvas
+            page.render(renderContext).then(function() {
+                __PAGE_RENDERING_IN_PROGRESS = 0;
+
+                // Re-enable Prev & Next buttons
+                $("#pdf-next, #pdf-prev").removeAttr('disabled');
+
+                // Show the canvas and hide the page loader
+                $("#pdf-canvas").show();
+                $("#page-loader").hide();
+                $("#download-image").show();
+            });
+        });
+    }
+
+    // Upon click this should should trigger click on the #file-to-upload file input element
+    // This is better than showing the not-good-looking file input element
+    $("#upload-button").on('click', function() {
+        $("#file-to-upload").trigger('click');
+    });
+
+    // When user chooses a PDF file
+    $("#file-to-upload").on('change', function() {
+        // Validate whether PDF
+        if(['application/pdf'].indexOf($("#file-to-upload").get(0).files[0].type) == -1) {
+            alert('Error : Not a PDF');
+            return;
+        }
+
+        // $("#upload-button").hide();
+
+        // Send the object url of the pdf
+        showPDF(URL.createObjectURL($("#file-to-upload").get(0).files[0]));
+    });
+
+    // Previous page of the PDF
+    $("#pdf-prev").on('click', function() {
+        if(__CURRENT_PAGE != 1)
+            showPage(--__CURRENT_PAGE);
+    });
+
+    // Next page of the PDF
+    $("#pdf-next").on('click', function() {
+        if(__CURRENT_PAGE != __TOTAL_PAGES)
+            showPage(++__CURRENT_PAGE);
+    });
+
+    // Hide the PDF
+    $("#pdf-hide").on('click', function() {
+        $("#pdf-canvas").hide();
+    });
+
+    // Show the PDF
+    $("#pdf-show").on('click', function() {
+        $("#pdf-canvas").show();
+    });
+
+    // Download button
+    // $("#download-image").on('click', function() {
+    //     $(this).attr('href', __CANVAS.toDataURL()).attr('download', 'page.png');
+    // });
