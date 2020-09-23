0dd197f2caee13c99a6991eaf87618a035ee9afe
KOU256 <kou256.8bit+github@gmail.com>
2020-09-23 17:04:45 +0900

    一旦整理した
    コメントアウトしている部分やjQueryを使っている部分を削除した。

--- dragdrop/script.js
+++ dragdrop/script.js
@@ -13,22 +13,6 @@ let fileNames = [];
 window.onload = () => {
   console.log("window on load");
 
-  // const video = document.querySelector("#camera");
-  // const canvas = document.querySelector("#picture");
-
-  // document.getElementById('file-sample').addEventListener('change', function (e) {
-  //   // 1枚だけ表示する
-  //   let file = e.target.files[0];
-  //   // ファイルのブラウザ上でのURLを取得する
-  //   let blobUrl = window.URL.createObjectURL(file);
-  //   // img要素に表示
-  //   let img = document.getElementById('picture');
-  //   img.src = blobUrl;
-  //   console.log("img update");
-  // });
-
-  // document.getElementById('loadFile').addEventListener('change', loadLocalFile);
-
   /** カメラ設定 */
   const constraints = {
     audio: false,
@@ -78,7 +62,6 @@ function handleFileSelect(evt) {
   // 以下に必要なFile Objectのプロパティを記述
   var output = [];
   for (var i = 0, f; f = files[i]; i++) {
-    // output.push('<li><strong>', escape(f.name),  f.type ,'</li>');
     if (/^image\/(png|jpeg|gif)$/.test(f.type)) {
       var fr = new FileReader();
       showIMGthumbnail(fr, f);
@@ -103,8 +86,7 @@ function showIMGthumbnail(fr, file) {
     this.tmpImg.onload = function () {
       document.getElementById('pdf-loader').style.display = "none";
       canvasCtx.drawImage(this, 0, 0, canvas.width, canvas.height);
-      drawFineName(canvasCtx, file.name);
-      // document.getElementById('drop_zone').appendChild(this);
+      drawFileName(canvasCtx, file.name);
     }
   }
 }
@@ -134,9 +116,6 @@ interact('.resize-drag')
         let y = (parseFloat(target.getAttribute('data-y')) || 0)
 
         // update the element's style
-        // target.style.width = event.rect.width + 'px'
-        // target.style.height = event.rect.height + 'px'
-
         let ratio = target.height / target.width;
         if (Math.abs(event.deltaRect.left) < Math.abs(event.deltaRect.top)) {
           target.style.width = event.rect.height / ratio + 'px'
@@ -202,40 +181,30 @@ function dragMoveListener(event) {
 // this function is used later in the resizing and gesture demos
 window.dragMoveListener = dragMoveListener
 
-  // __PAGE_RENDERING_IN_PROGRESS = 0;
-
 function showPDF(pdfUrl) {
-  // $("#pdf-loader").show();
   currentMaterialIndex = document.getElementsByClassName('resize-drag').length;
   let canvas = document.createElement("canvas");
   canvas.classList.add('resize-drag');
-  // canvas.setAttribute('width', 400);
   canvas.width = 400;
   canvas.dataset.materialIndex = currentMaterialIndex;
 
   PDFJS.getDocument({ url: pdfUrl }).then(function (pdfDoc) {
-    // __PDF_DOC = pdf_doc;
     materials.push(pdfDoc);
     let numPages = pdfDoc.numPages;
     canvas.dataset.numPages = numPages;
-
-    // Hide the pdf loader and show pdf container in HTML
-    // $("#pdf-loader").hide();
-    // $("#pdf-contents").show();
-    // $("#pdf-total-pages").text(__NUM_PAGES);
-
     // Show the first page
     canvas.dataset.page = 1;
     document.body.appendChild(canvas);
     showPage(1);
+
     document.getElementById('pdf-hide').removeAttribute('disabled');
     document.getElementById('pdf-show').removeAttribute('disabled');
     document.getElementById('pdf-delete').removeAttribute('disabled');
     showPDFthumbnail(); // ここにおかないとダメっぽい
   }).catch(function (error) {
     // If error re-show the upload button
-    $("#pdf-loader").hide();
-    $("#upload-button").show();
+    document.getElementById('pdf-loader').hide();
+    document.getElementById('upload-button').show();
 
     alert(error.message);
   });
@@ -244,7 +213,6 @@ function showPDF(pdfUrl) {
 }
 
 function showPDFthumbnail () {
-  // currentMaterialIndex = document.getElementsByClassName('resize-drag').length;
   let canvas = createThumbnailCanvas();
   let canvasCtx = canvas.getContext('2d');
 
@@ -255,9 +223,6 @@ function showPDFthumbnail () {
     // Get viewport of the page at required scale
     let viewport = page.getViewport(scaleRequired);
 
-    // Set canvas height
-    // canvas.width = viewport.width;
-
     let renderContext = {
       canvasContext: canvasCtx,
       viewport: viewport
@@ -266,7 +231,7 @@ function showPDFthumbnail () {
     document.getElementById('pdf-loader').style.display = "none";
     // Render the page contents in the canvas
     page.render(renderContext).then(function () {
-      drawFineName(canvasCtx, fileNames[currentMaterialIndex]);
+      drawFileName(canvasCtx, fileNames[currentMaterialIndex]);
     });
   });
 }
@@ -280,7 +245,7 @@ function createThumbnailCanvas() {
   return canvas;
 }
 
-function drawFineName(canvasCtx, fileName) {
+function drawFileName(canvasCtx, fileName) {
   canvasCtx.fillStyle = "white";        // 塗りつぶす色
   canvasCtx.fillRect(0, 75, 100, 25);    // 描画
   // ファイル名描画
@@ -295,7 +260,6 @@ function showPage(pageNo) {
   let canvas = document.getElementsByClassName('resize-drag')[currentMaterialIndex],
     canvasCtx = canvas.getContext('2d');
 
-  // __PAGE_RENDERING_IN_PROGRESS = 1;
     canvas.dataset.page = pageNo;
 
   // Disable Prev & Next buttons while page is being loaded
@@ -310,16 +274,6 @@ function showPage(pageNo) {
     document.getElementById('pdf-next').removeAttribute('disabled');
   }
 
-  // While page is being rendered hide the canvas and show a loading message
-  // $("#pdf-canvas").hide();
-  // $("#page-loader").show();
-  // $("#download-image").hide();
-
-  // Update current page in HTML
-  // $("#pdf-current-page").text(pageNo);
-
-  // Fetch the page
-  // __PDF_DOC.getPage(page_no).then(function (page) {
     materials[currentMaterialIndex].getPage(pageNo).then(function (page) {
     // As the canvas is of a fixed width we need to set the scale of the viewport accordingly
     let scaleRequired = canvas.width / page.getViewport(1).width;
@@ -336,14 +290,7 @@ function showPage(pageNo) {
     };
 
     // Render the page contents in the canvas
-    page.render(renderContext).then(function () {
-      // __PAGE_RENDERING_IN_PROGRESS = 0;
-
-      // Show the canvas and hide the page loader
-      // $("#pdf-canvas").show();
-      // $("#page-loader").hide();
-      // $("#download-image").show();
-    });
+    page.render(renderContext);
   });
 }
 
@@ -375,12 +322,12 @@ function showImage(imgUrl) {
 
 // Upon click this should should trigger click on the #file-to-upload file input element
 // This is better than showing the not-good-looking file input element
-$("#upload-button").on('click', function () {
-  $("#file-to-upload").trigger('click');
-});
+document.getElementById('upload-button').onclick = function () {
+  document.getElementById('file-to-upload').trigger('click');
+}
 
 // When user chooses a PDF file
-$("#file-to-upload").on('change', function () {
+document.getElementById('file-to-upload').onchange = function () {
   const file = document.getElementById('file-to-upload').files[0];
   console.log(file);
   switch (file.type) {
@@ -396,36 +343,36 @@ $("#file-to-upload").on('change', function () {
       alert('Error : Not a PDF or Image');
       break;
   }
-});
+}
 
 // Previous page of the PDFï￥
-$("#pdf-prev").on('click', function () {
+document.getElementById('pdf-prev').onclick = function () {
   let material = document.getElementsByClassName('resize-drag')[currentMaterialIndex],
     currentPage = parseInt(material.dataset.page);
   if (currentPage !== 1) {
     showPage(--currentPage);
   }
-});
+}
 
 // Next page of the PDF
-$("#pdf-next").on('click', function () {
+document.getElementById('pdf-next').onclick = function () {
   let material = document.getElementsByClassName('resize-drag')[currentMaterialIndex],
     currentPage = parseInt(material.dataset.page),
     numPages = parseInt(material.dataset.numPages);
   if (currentPage !== numPages) {
     showPage(++currentPage);
   }
-});
+}
 
 // Hide the PDF
-$("#pdf-hide").on('click', function () {
-  $("#pdf-canvas").hide();
-});
+document.getElementById('pdf-hide').onclick = function () {
+  document.getElementById('pdf-canvas').hide();
+}
 
 // Show the PDF
-$("#pdf-show").on('click', function () {
-  $("#pdf-canvas").show();
-});
+document.getElementById('pdf-show').onclick = function () {
+  document.getElementById('pdf-canvas').show();
+}
 
 document.getElementById('pdf-delete').onclick = function() {
   let canvases = document.getElementsByClassName('resize-drag');
@@ -438,15 +385,9 @@ document.getElementById('pdf-delete').onclick = function() {
   currentMaterialIndex = 0;
 }
 
-// Download button
-// $("#download-image").on('click', function() {
-//     $(this).attr('href', __CANVAS.toDataURL()).attr('download', 'page.png');
-// });
-
 async function main() {
   // Load the MediaPipe handpose model assets.
   const landmark_model = await handpose.load();
-  // const gesture_model = tf.loadModel('./model/model.json');
 
   const video = document.getElementById('camera');
   const canvas = document.getElementById('mask');
